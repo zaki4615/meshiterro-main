54e8f203cad3fdfd8e6e579a7f15cdc7434e19d5
Wataru Kanzaki <224184043+zaki4615@users.noreply.github.com>
2025-10-11 11:30:21 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get 'homes/about', to: 'homes#about', as: :about
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+end
